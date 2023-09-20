7affadf7cf70deaf8134203ae78f800c9644ef26
reneastudio <118086824+reneastudio@users.noreply.github.com>
2023-09-20 12:43:38 +0700

    Update language

--- js/app.js
+++ js/app.js
@@ -45,6 +45,12 @@ $(document).ready(function () {
       });
     });
 
+    // GOOGLE TRANSLATE BAR HIDE
+    if(document.getElementsByClassName('.skiptranslate')[0] !== undefined) {
+      document.getElementsByClassName('.skiptranslate')[0].style.display  = 'none';
+      document.body.style.top = '0px';
+    }
+
     // FAQs ACCORDION
     (function Accordion() {
       const triggers = document.querySelectorAll('[data-toggle="collapse"]');
@@ -138,3 +144,26 @@ $(document).ready(function () {
     }
 
     requestAnimationFrame(raf);
+
+    
+    // WHATSAPP CHAT
+    /* Whatsapp Chat Widget by www.idblanter.com */
+    $(document).on("click", "#send-it", function () {
+      var a = document.getElementById("chat-input");
+      if ("" != a.value) {
+          var b = $("#get-number").text(),
+              c = document.getElementById("chat-input").value,
+              d = "https://web.whatsapp.com/send",
+              e = b,
+              f = "&text=" + c;
+          if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) var d = "whatsapp://send";
+          var g = d + "?phone=" + e + f;
+          window.open(g, '_blank')
+      }
+    }), $(document).on("click", ".informasi", function () {
+      document.getElementById("get-number").innerHTML = $(this).children(".my-number").text(), $(".start-chat,.get-new").addClass("show").removeClass("hide"), $(".home-chat,.head-home").addClass("hide").removeClass("show"), document.getElementById("get-nama").innerHTML = $(this).children(".info-chat").children(".chat-nama").text(), document.getElementById("get-label").innerHTML = $(this).children(".info-chat").children(".chat-label").text()
+    }), $(document).on("click", ".close-chat", function () {
+      $("#whatsapp-chat").addClass("hide").removeClass("show")
+    }), $(document).on("click", ".konsultasi, .blantershow-chat", function () {
+      $("#whatsapp-chat").addClass("show").removeClass("hide")
+    });
