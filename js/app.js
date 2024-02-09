c8baedaf432a08b1b0f39dd20a471a3b5a188fe2
reneastudio <118086824+reneastudio@users.noreply.github.com>
2024-02-09 18:39:54 +0700

    Redesign everything

--- js/app.js
+++ js/app.js
@@ -1,31 +1,22 @@
 $(document).ready(function () {
-      function showTime() {
-        var date = new Date();
-        var h = date.getHours(); // 0 - 23
-        var m = date.getMinutes(); // 0 - 59
-        var s = date.getSeconds(); // 0 - 59
-        var session = "AM";
+  function showTime() {
+    var options = {
+      timeZone: 'Asia/Jakarta',
+      hour12: false, // 24-hour format
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    };
+    
+    var jakartaTime = new Date().toLocaleString('en-US', options);
+    
+    document.getElementById("jam").innerText = jakartaTime;
+    document.getElementById("jam").textContent = jakartaTime;
 
-        if (h == 0) {
-          h = 12;
-        }
+    setTimeout(showTime, 1000);
+  }
+  showTime();
 
-        if (h > 12) {
-          h = h - 12;
-          session = "PM";
-        }
-
-        h = h < 10 ? "0" + h : h;
-        m = m < 10 ? "0" + m : m;
-        s = s < 10 ? "0" + s : s;
-
-        var time = h + ":" + m + ":" + s;
-        document.getElementById("jam").innerText = time;
-        document.getElementById("jam").textContent = time;
-
-        setTimeout(showTime, 1000);
-      }
-      showTime();
 
       // OWL Carousel
       var owl = $(".owl-carousel");
